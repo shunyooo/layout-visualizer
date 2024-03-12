e6c5701016d91c3879267d815c2ab2b16666aaba
Shunyo Kawamoto <shunyo.kawamoto@gmail.com>
2024-03-12 10:34:35 +0000

    Update colormap example

--- layout_visualizer/__init__.py
+++ layout_visualizer/__init__.py
@@ -1,3 +1,3 @@
-from .layout_visualizer import draw_label_bboxes
+from .layout_visualizer import Color, draw_label_bboxes
 
-__all__ = ["draw_label_bboxes"]
+__all__ = ["draw_label_bboxes", "Color"]
